e8af92b760306fe48e069edd24f3f5b2868907a0
dingdz <ddzyan@163.com>
2018-10-31 19:27:33 +0800

    添加样式，选择功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { TranslateService } from "@ngx-translate/core";
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { TabsPage } from "../pages/tabs/tabs";
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    public translate: TranslateService
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      translate.addLangs(["en", "zh"]);
+      translate.setDefaultLang("en");
+
+      const browserLang = translate.getBrowserLang();
+      translate.use(browserLang.match(/en|zh/) ? browserLang : "en");
       statusBar.styleDefault();
       splashScreen.hide();
     });
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,14 +7,24 @@ import { TranslateService } from "@ngx-translate/core";
   templateUrl: "home.html"
 })
 export class HomePage {
+  private languages = [
+    {
+      name: "English",
+      isoCode: "en"
+    },
+    {
+      name: "中文（简体）",
+      isoCode: "zh",
+      useIdeograms: true
+    }
+  ];
+
   constructor(
     public navCtrl: NavController,
     public translate: TranslateService
-  ) {
-    translate.addLangs(["en", "fr"]);
-    translate.setDefaultLang("en");
+  ) {}
 
-    const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|fr/) ? browserLang : "en");
+  public save(newLang: string): void {
+    this.translate.use(newLang);
   }
 }
